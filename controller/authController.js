6f50259a0902449ad399cc71a72e8ff071818b28
FruitfulEjiro <frutfl@outlook.com>
2025-03-17 16:46:15 +0100

    Update password

--- controller/authController.js
+++ controller/authController.js
@@ -9,20 +9,20 @@ const generateToken = (userID) => {
    return jwt.sign({ id: userID }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 };
 
-const createSendToken = AsyncHandler(async (user, res) => {
-   const token = await generateToken(user._id);
+const createSendToken = AsyncHandler(async (user, res, code) => {
+   const token = generateToken(user._id);
    const cookieOptions = {
-      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000),
+      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
       httpOnly: true,
    };
    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
    user.password = undefined;
 
-   return res.status(201).cookie("jwt", token, cookieOptions).json({
+   return res.status(code).cookie("jwt", token, cookieOptions).json({
       status: "success",
       message: "Successful",
-      user
+      user,
    });
 });
 
@@ -32,7 +32,7 @@ export const signup = AsyncHandler(async (req, res, next) => {
    const user = await User.create({ name: { firstname, lastname }, email, password });
    if (!user) return next(new AppError("User not created", 400));
 
-   await createSendToken(user, res);
+   await createSendToken(user, res, 201);
    return; // Ensure no further processing occurs
 });
 
@@ -47,7 +47,7 @@ export const login = AsyncHandler(async (req, res, next) => {
    }
 
    // Create and send token
-   await createSendToken(user, res);
+   await createSendToken(user, res, 200);
    return; // Ensure no further processing occurs
 });
 
@@ -63,7 +63,7 @@ export const protect = AsyncHandler(async (req, res, next) => {
    }
 
    // verify JWT
-   const decoded = jwt.verify(token, process.env.JWT_SECRET);
+   const decoded = await jwt.verify(token, process.env.JWT_SECRET);
    if (!decoded) {
       return res.status(401).json({
          status: "Failed",
@@ -95,4 +95,35 @@ export const authorize = AsyncHandler(async (req, res, next) => {
       });
    }
    next();
+});
+
+export const updatePassword = AsyncHandler(async (req, res, next) => {
+   const { id, currentPassword, newPassword } = req.body;
+
+   // get user from DB
+   const user = await User.findById(id).select("+password");
+   console.log(user);
+
+   // check if password is correct
+   if (!(await user.correctPassword(currentPassword, user.password))) {
+      return next(new AppError("Your current Password is wrong", 401));
+   }
+
+   user.password = newPassword;
+   await user.save();
+
+   createSendToken(user, res, 200);
+});
+
+export const logout = AsyncHandler(async (req, res, next) => {
+   res.status(200)
+      .cookie("jwt", "Logged out", {
+         expires: new Date(Date.now() + 5 * 1000),
+         httpOnly: true,
+      })
+      .json({
+         status: "success",
+         message: "Log out Successful",
+         redirect: "/",
+      });
 });
